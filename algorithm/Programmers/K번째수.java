494f695fb92c90eb84ea49146979e214d1fe0698
SeungWooL <xonic789@naver.com>
2021-06-13 23:31:44 +0900

    프로그래머스 2문제 해결

---
+++ algorithm/Programmers/K번째수.java
@@ -0,0 +1,34 @@
+package algorithm.Programmers;
+
+import java.util.*;
+
+public class K번째수 {
+    public static List<Integer> solution(int[] array, int[][] commands) {
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < commands.length; i++){
+            int from = commands[i][0] - 1;
+            int to = commands[i][1] - 1;
+            Integer[] arr = new Integer[to - from + 1];
+            for (int j = 0; j < arr.length; j++){
+                arr[j] = array[from++];
+            }
+            Arrays.sort(arr,(o1, o2) -> o1 - o2);
+            answer.add(arr[commands[i][2] - 1]);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {
+                {2, 5, 3},
+                {4, 4, 1},
+                {1, 7, 3}
+        };
+
+
+        System.out.println(solution(array,commands));
+    }
+}
