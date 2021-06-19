820770127685177e4b70968734c01d2311ecfd50
SeungWooL <xonic789@naver.com>
2021-06-19 16:20:21 +0900

    간단한 정수형 집합 구현

--- algorithm/inflearn/tree/송아지찾기.java
+++ algorithm/inflearn/tree/송아지찾기.java
@@ -1,4 +1,43 @@
 package algorithm.inflearn.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class 송아지찾기 {
+    public int solution(int root, int target){
+        int answer = 0;
+        answer = BFS(root,target);
+        return answer;
+    }
+
+    public int BFS(int root, int target){
+        int L = 0;
+        boolean[] check = new boolean[10001];
+        int[] move = {1, -1, 5};
+        Queue<Integer> queue = new LinkedList<>();
+        check[root] = true;
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int len = queue.size();
+            for (int i = 0; i < len; i++){
+                int x = queue.poll();
+                for (int j = 0; j < 3; j++){
+                    int nx = x + move[j];
+                    if (nx==target) return L+1;
+                    if (nx >= 1 && nx <= 10000 && !check[nx]){
+                        check[nx] = true;
+                        queue.offer(nx);
+                    }
+                }
+            }
+            L++;
+        }
+
+        return L;
+    }
+
+    public static void main(String[] args) {
+        송아지찾기 solution = new 송아지찾기();
+        System.out.println(solution.solution(5,14));
+    }
 }
---
+++ dataStructure/Set.java
@@ -0,0 +1,90 @@
+package dataStructure;
+
+import java.util.Iterator;
+
+/**
+ * 기본적인 집합 자료구조 입니다.
+ */
+public class Set  {
+    private int max;
+    private int[] set;
+    private int num;
+
+
+    public Set(int[] array){
+        this(array.length);
+        for (int i = num; i < max; i++){
+           if (!contains(array[i])){
+               set[num] = array[i];
+               num++;
+           }
+        }
+    }
+
+    public Set(int capacity){
+        max = capacity;
+        num = 0;
+        try {
+            set = new int[max];
+        }catch (OutOfMemoryError e){
+            max = 0;
+        }
+    }
+
+    public Set(){
+        max = 10;
+        num = 0;
+        try {
+            set = new int[max];
+        }catch (OutOfMemoryError e){
+            max = 0;
+        }
+    }
+
+    public boolean add(int element){
+        if (num + 1 == max){
+            copyArray();
+        }
+        if (!contains(element)){
+            set[num++] = element;
+            return true;
+        }
+        return false;
+    }
+
+    public void copyArray(){
+        max *= 2;
+        int[] tmp = new int[max];
+        for (int i = 0; i < max; i++){
+            tmp[i] = set[i];
+        }
+        set = tmp;
+    }
+
+    public boolean contains(int element){
+        return (indexOf(element) != -1) ? true : false;
+    }
+
+    public int indexOf(int element){
+        for (int i = 0; i < num; i++){
+            if (set[i] == element){
+                return element;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < num; i++){
+            sb.append(set[i] + ",");
+        }
+        String string = sb.substring(0,sb.length()-1);
+        string += "]";
+        return string;
+
+    }
+
+}
