da703499c456f636229009c4e03f9b3c5c3b4761
SeungWooL <xonic789@naver.com>
2021-06-30 21:54:39 +0900

    투포인터스 알고리즘

--- algorithm/boostcamp/Main.java
+++ algorithm/boostcamp/Main.java
@@ -1,9 +1,41 @@
 package algorithm.boostcamp;
 
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] arr = new int[n];
 
+        for (int i = 0; i < n; i++){
+            arr[i] = in.nextInt();
+        }
+        int lt = 0;
+        int rt = k - 1;
+        int sum = 0;
+
+        for (int i = 0; i < k; i++){
+            sum += arr[i];
+        }
+
+
+        int answer = sum;
+        while (rt < n){
+            sum -= arr[lt++];
+            sum += arr[rt++];
+            if (sum > answer){
+                answer = sum;
+            }
+        }
+
+        System.out.println(answer);
+
+
+        return ;
+    }
 }
-
 
 
--- algorithm/inflearn/timeComplexity/최대_길이_연속부분수열.java
+++ algorithm/inflearn/timeComplexity/최대_길이_연속부분수열.java
@@ -1,11 +1,36 @@
 package algorithm.inflearn.timeComplexity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class 최대_길이_연속부분수열 {
 
     static int solution(int n,int k,int[] input){
         int answer = 0;
+        List<Integer> list = new ArrayList<>();
+        int lt = 0;
+        int rt = 0;
+        int cnt = 0;
+
+        while (rt < n){
+            // rt가 0일 때는 무조건 cnt++해준다.
+            if (input[rt] == 0){
+                cnt++;
+            }
+            // cnt가 k보다 크다면 lt를 0일 때 까지 증가시켜준다.
+            if (k < cnt){
+                while (cnt > k){
+                    if (input[lt] == 0) cnt--;
+                    lt++;
+                }
+            }
+            if (answer < rt - lt + 1){
+                answer = rt - lt + 1;
+            }
+            rt++;
+
+        }
         
 
         return answer;
