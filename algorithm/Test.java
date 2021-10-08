49b327039adf7cb2fe2701648ca1e8a7f92cc082
xonic789 <xonic789@naver.com>
2021-10-08 22:55:53 +0900

    test

--- algorithm/Test.java
+++ algorithm/Test.java
@@ -6,8 +6,11 @@ import java.util.*;
 public class Test {
 
 
-
-
+    public static void main(String[] args) {
+        List<String> l = new ArrayList<>();
+        l.stream().forEach(System.out::println);
+        StringBuilder sb = new StringBuilder();
+    }
 
 
 }
