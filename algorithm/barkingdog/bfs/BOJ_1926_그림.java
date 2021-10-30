ea44f8a4f908c60c6344daffc899ee59cf62441a
xonic789 <xonic789@naver.com>
2021-10-30 22:41:02 +0900

    BOJ 문제풀이

---
+++ algorithm/barkingdog/bfs/BOJ_1926_그림.java
@@ -0,0 +1,26 @@
+package algorithm.barkingdog.bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_1926_그림 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] graph = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+
+    }
+}
--- algorithm/goorm/Goorm_BinarySearch.java
+++ algorithm/goorm/Goorm_BinarySearch.java
@@ -1,2 +1,35 @@
-package algorithm.goorm;public class Goorm_BinarySearch {
+package algorithm.goorm;
+
+import java.io.*;
+
+public class Goorm_BinarySearch {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        String[] inputs = br.readLine().split(" ");
+        for (int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(inputs[i]);
+        }
+        int target = Integer.parseInt(br.readLine());
+        int answer = binarySearch(arr, target);
+        if (answer != -1) {
+            System.out.println(answer);
+        } else {
+            System.out.println("X");
+        }
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int lt = 0, rt = arr.length - 1;
+
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (target == arr[mid]) return mid + 1;
+            if (arr[mid] > target) rt = mid - 1;
+            else if (arr[mid] < target) lt = mid + 1;
+        }
+        return -1;
+    }
 }
