5d4771c37e81f00aa35bb572db91cf7b18ea35e3
xonic789 <xonic789@naver.com>
2022-03-02 23:41:35 +0900

    스프링 MVC Thymeleaf 사용법
    템플릿 조각, 템플릿 조각 확장

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -15,6 +15,11 @@ public class TemplateController {
 
     @GetMapping("/layout")
     public String layout() {
-        return "template/layout";
+        return "template/layout/layoutMain";
+    }
+
+    @GetMapping("/layoutExtend")
+    public String layoutExtend() {
+        return "template/layoutExtend/layoutExtendMain";
     }
 }
