e07c2599ff0dfd033c37190f04beea6db7da8fe3
xonic987 <dltmddn951@rubric.im>
2022-03-24 22:20:32 +0900

    스프링 MVC 로그인
    쿠키, 쿠키와 보안 문제 -> 대안

--- springFramework/springMVC2/login/src/main/java/hello/login/domain/member/MemberRepository.java
+++ springFramework/springMVC2/login/src/main/java/hello/login/domain/member/MemberRepository.java
@@ -18,17 +18,21 @@ public class MemberRepository {
         store.put(member.getId(), member);
         return member;
     }
+
     public Member findById(Long id) {
         return store.get(id);
     }
+
     public Optional<Member> findByLoginId(String loginId) {
         return findAll().stream()
                 .filter(m -> m.getLoginId().equals(loginId))
                 .findFirst();
     }
+
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
+
     public void clearStore() {
         store.clear();
     }
