9676d10710b0ebbbce3cf30e25da6f1b57fa0f98
xonic789 <xonic789@naver.com>
2022-04-16 21:38:50 +0900

    스프링 MVC 파일업로드
    HttpServletRequest의 getParts(); 메서드를 호출하면 바이너리 파일로 보낸 데이터를 받을 수 있다.

---
+++ springFramework/springMVC2/uploader/src/main/java/hello/uploader/UploaderApplication.java
@@ -0,0 +1,13 @@
+package hello.uploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploaderApplication.class, args);
+	}
+
+}
---
+++ springFramework/springMVC2/uploader/src/test/java/hello/uploader/UploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.uploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
