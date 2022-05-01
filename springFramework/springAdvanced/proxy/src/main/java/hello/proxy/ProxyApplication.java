70555fd59e9e15ce2a35292aeaa1571f585395cc
xonic789 <xonic789@naver.com>
2022-05-01 23:22:51 +0900

    jdk 동적 프록시와 CGLIB, 그리고 스프링이 더 편리하게 제공하는 포인트컷, 어드바이스, 어드바이저

--- springFramework/springAdvanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ springFramework/springAdvanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,10 +1,7 @@
 package hello.proxy;
 
-import hello.proxy.config.AppV1Config;
-import hello.proxy.config.AppV2Config;
-import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
-import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
-import hello.proxy.config.v2_dynamicproxy.handler.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -19,7 +16,7 @@ import org.springframework.context.annotation.Import;
 //})
 //@Import(InterfaceProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
-@Import(DynamicProxyBasicConfig.class)
+@Import(DynamicProxyFilterConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
