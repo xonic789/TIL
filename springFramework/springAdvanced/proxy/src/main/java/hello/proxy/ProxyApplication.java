38c94c9f0b63eefaf3fa177a30660aa33543d3b3
xonic789 <xonic789@naver.com>
2022-05-02 01:36:40 +0900

    포인트컷, 어드바이스, 어드바이저와 빈 후처리기

--- springFramework/springAdvanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ springFramework/springAdvanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,6 +2,9 @@ package hello.proxy;
 
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -16,7 +19,10 @@ import org.springframework.context.annotation.Import;
 //})
 //@Import(InterfaceProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
-@Import(DynamicProxyFilterConfig.class)
+//@Import(DynamicProxyFilterConfig.class)
+//@Import(ProxyFactoryConfigV1.class)
+//@Import(ProxyFactoryConfigV2.class)
+@Import(BeanPostProcessorConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
