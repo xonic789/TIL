7f647caec1e9b5a6a80ac35d79a82d7396735374
ian <xonic789@naver.com>
2024-05-26 22:04:47 +0900

    PS-E: 88. Merge Sorted Array - Two Pointers 알고리즘으로 변경. - non-decreasing order는 숫자가 증가하거나 증가하지 않을 수 있지만, 결코 감소하지 않는 경우이므로, nums1, num2는 정렬되어있다는 것을 알 수 있다.

--- algorithm/leetcode/kotlin/MergeSortedArray88.kt
+++ algorithm/leetcode/kotlin/MergeSortedArray88.kt
@@ -2,34 +2,30 @@ import java.util.*
 
 class MergeSortedArray88 {
     fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
-        // return 이 없고 nums1을 직접 수정한다.
-        // m 은 nums1 에 원소의 수, n 은 nums2 에 원소의 수
-        // m - nums.size
-        // 6 - 3 = 3 -> 3번째부터 nums.size까지
-        //
-        if (n == 0) {
-            Arrays.sort(nums1, 0, nums1.size)
-            return
-        }
-        if (m == 0) {
-            for (i in 0 until n) {
-                nums1[i] = nums2[i]
+        var p1 = m - 1 // nums1의 마지막 요소 인덱스
+        var p2 = n - 1 // nums2의 마지막 요소 인덱스
+        var p = m + n - 1 // nums1의 병합 결과를 저장할 위치
+        // 참고: 해당 문제에서 non-decreasing order는 숫자가 증가하거나 증가하지 않을 수 있지만 결코 감소하지 않는 경우이다.
+        // 예: 1, 2, 2, 3, 4, 5, 6
+        // merge sort 중 merge 부분
+
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1--]
+            } else {
+                nums1[p] = nums2[p2--]
             }
-            Arrays.sort(nums1, 0, nums1.size)
-            return
+            p--
         }
 
-        for (i in m until nums1.size) {
-            nums1[i] = nums2[i - m]
+        // nums2에 남은 요소가 있는 경우
+        while (p2 >= 0) {
+            nums1[p--] = nums2[p2--]
         }
-
-        Arrays.sort(nums1, 0, nums1.size)
-        nums1.forEach { print("$it ") }
     }
 }
 
 fun main() {
-    // 10^9 = 1000000000
     MergeSortedArray88().merge(intArrayOf(1, 2, 3, 0, 0, 0), 3, intArrayOf(2, 5, 6), 3)
 
 }
