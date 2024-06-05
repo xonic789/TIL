eb024a37e38dbc0bf1111ce7c508f57ab126a65e
xonic <xonic789@naver.com>
2024-06-05 11:52:29 +0900

    함수 위치 리팩토링

--- algorithm/leetcode/kotlin/RemoveDuplicatesFromSortedArray26.kt
+++ algorithm/leetcode/kotlin/RemoveDuplicatesFromSortedArray26.kt
@@ -1,26 +1,26 @@
 import java.util.*
 
 class RemoveDuplicatesFromSortedArray26 {
-}
-
-
-fun removeDuplicates(nums: IntArray): Int {
-    val table = mutableMapOf<Int, Int>()
-    var notContainNum = 101
-    for (i in nums.indices) {
-        val num = nums[i]
-        if (table[num] == null) {
-            table[num] = 1
-        } else {
-            nums[i] = notContainNum
+    fun removeDuplicates(nums: IntArray): Int {
+        val table = mutableMapOf<Int, Int>()
+        var notContainNum = 101
+        for (i in nums.indices) {
+            val num = nums[i]
+            if (table[num] == null) {
+                table[num] = 1
+            } else {
+                nums[i] = notContainNum
+            }
         }
+        Arrays.sort(nums)
+        return table.count()
     }
-    Arrays.sort(nums)
-    return table.count()
 }
+
+
 
 fun main() {
     val nums = intArrayOf(0,0,1,1,1,2,2,3,3,4)
-    println(removeDuplicates(nums))
+    println(RemoveDuplicatesFromSortedArray26().removeDuplicates(nums))
     println(nums.toList())
 }
--- algorithm/leetcode/kotlin/RemoveElement27.kt
+++ algorithm/leetcode/kotlin/RemoveElement27.kt
@@ -1,42 +1,42 @@
 import java.util.*
 
 class RemoveElement27 {
+    fun removeElement(nums: IntArray, `val`: Int): Int {
+        val notContainNumber = 51
+        for (i in nums.indices) {
+            if (nums[i] == `val`) {
+                nums[i] = notContainNumber
+            }
+        }
+
+        // -1을 배열 끝으로 이동
+        var left = 0
+        var right = nums.size - 1
+        while (left < right) {
+            if (nums[left] == notContainNumber) {
+                nums[left] = nums[right]
+                nums[right] = notContainNumber
+                right--
+            } else {
+                left++
+            }
+        }
+
+        return nums.count{it != notContainNumber}
+    }
 }
 
-fun removeElement(nums: IntArray, `val`: Int): Int {
-    val notContainNumber = 51
-    for (i in nums.indices) {
-        if (nums[i] == `val`) {
-            nums[i] = notContainNumber
-        }
-    }
-
-    // -1을 배열 끝으로 이동
-    var left = 0
-    var right = nums.size - 1
-    while (left < right) {
-        if (nums[left] == notContainNumber) {
-            nums[left] = nums[right]
-            nums[right] = notContainNumber
-            right--
-        } else {
-            left++
-        }
-    }
-
-    return nums.count{it != notContainNumber}
-}
 
 
 fun main() {
     var nums = intArrayOf(3, 2, 2, 3)
     val `val` = 3
-    println(removeElement(nums, `val`))
+    println(RemoveElement27().removeElement(nums, `val`))
     println(nums.contentToString())
 
     var nums1 = intArrayOf(0,1,2,2,3,0,4,2)
     val `val1` = 2
     Arrays.sort(nums1, 0, nums1.size);
-    println(removeElement(nums1, `val1`))
+    println(RemoveElement27().removeElement(nums1, `val1`))
     println(nums1.contentToString())
 }
